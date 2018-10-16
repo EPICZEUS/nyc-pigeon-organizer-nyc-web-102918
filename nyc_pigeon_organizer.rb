f9b8e07850d43a05ea4c6036b1524cea5c15451e
Sam B <s77broz@aol.com>
2018-10-16 17:58:32 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
 def nyc_pigeon_organizer(data)
-  
+  out = {}
+  data.each do |key, val|
+    val.each do |k, v|
+      v.each do |birb|
+        unless out[birb]
+          out[birb] = {}
+        end
+        
+        unless out[birb][key]
+          out[birb][key] = []
+        end
+        
+        out[birb][key] << k.to_s
+      end
+    end
+  end
 end
